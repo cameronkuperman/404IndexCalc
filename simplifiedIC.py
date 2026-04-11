e70e1e36ddbe4c759a9daba763f2ce0ee7ec0f44
Cameron Kuperman <ckk23@duke.edu>
2026-04-11 19:14:08 -0400

    small fixes

--- simplifiedIC.py
+++ simplifiedIC.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 from math import exp, sqrt, log
 import math
+import sympy
 class IndexCalc:
     def __init__(self, prime, g, ):
         self.p = prime
@@ -13,33 +14,48 @@ class IndexCalc:
 
         return (b, primeList)
      #tuple of the list and integer
+    def factor(self, num):
+        return sympy.factorint(num)
     def computeAllFactors(self, b):
-        return primesieve.generate_primes(b)
+        return primesieve.primes(b)
     def computeFactorBaseLogs(self, p ):
         rank= 0 
         matrix = np.zeros((len(pl), len(pl)))
         b, pl = self.computeFactorBase(p)
         while (rank<len(pl)):
             k = random.randint(self.p)
-            if max(self.computeAllFactors(self.g**k % p)) > max(pl):
+            num = self.g**k % p 
+            if max(self.computeAllFactors(num)) > max(pl):
                 continue
-            matrix[rank]=factors of the b smooth # we get
+            matrix[rank]=self.createLogVector(num)
             if np.linalg.matrix_rank(matrix)>rank:
+                rank+=1
                 continue
+            matrix[rank]= np.zeros(len(pl))
+            
         logsolutions = np.linalg.solve(matrix)
 
         return {pl[i]: logsolutions[i] for i in range(len(pl))}
-        #hardest part 
-        # essentially randomly sample ks mod p-1 and take to the power of k using a set / other datra structure to ensure onrepeats
-        #do this len(b smoot factors) times andthen take detemrinient of this matrix to see if linearly independent like btw t
-        #btw you have been building a matrix this whole time take determinent at the end to see if lin ind
-        #or you can do like 5len(size) times and just think it will work out
-        #row reduction seems ideal ok and 
 
-    def createLogVector(self, num):
-        self.computeAllFactors(num)
+
+    def createLogVector(self, num, baseList):
+        dc =  self.factor(num)
+        
+
+        lst2 = [dc.get(k,0) for k in sorted(baseList)]
+
+        return lst2
+
+
+            
 
         return #dict of the actual factor base logs e.g. like factors etc... and their corresponding logs
-    def solveForX(self, b):
-        #get the linear combinartion of primes in there and return it
-        return
+    def solveForX(self, num ):
+        pfactordict = self.computeFactorBaseLogs(self.p)
+        factors = sympy.factorint(num % self.p)
+        x=0
+        for key in factors:
+            x+=pfactordict[key]*factors[key]
+
+
+        return x
